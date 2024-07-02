a0d0c9582be949b35a1007421fcea7a2c24045cc
James Brady <james@jmsbrdy.com>
2024-07-02 14:16:37 +0100

    build site

---
+++ out/_next/static/MQXV2AzhYu2hkjWQO6D4_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/221-6812c53702cb54b2.js","static/chunks/pages/index-ffc3fe50b4997379.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
